c72ab33cab91e19695d1427371f0d2aecbcf3108
ktafro1 <ktafro1@etf.unsa.ba>
2020-03-11 11:31:29 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Label poruka;
+
+    public void reciHello(ActionEvent actionEvent) {
+    }
+}
